ce0eccf97715c2cb0874c4f25382460b925f76fb
Sean McArthur <sean@seanmonstar.com>
2019-09-13 11:21:31 -0700

    move Error into error module

---
+++ src/error.rs
@@ -0,0 +1,64 @@
+use std::fmt;
+
+/// Error type when failing to parse or build a `Cookie`.
+#[derive(Debug)]
+pub struct Error {
+    kind: Kind,
+}
+
+#[derive(Debug)]
+enum Kind {
+    InvalidName,
+    InvalidValue,
+    InvalidPath,
+    InvalidDomain,
+    TooLong,
+}
+
+// ===== impl Error =====
+
+impl Error {
+    pub(crate) fn invalid_name() -> Error {
+        Error { kind: Kind::InvalidName }
+    }
+
+    pub(crate) fn invalid_value() -> Error {
+        Error { kind: Kind::InvalidValue }
+    }
+
+    pub(crate) fn invalid_path() -> Error {
+        Error { kind: Kind::InvalidPath }
+    }
+
+    pub(crate) fn invalid_domain() -> Error {
+        Error { kind: Kind::InvalidDomain }
+    }
+
+    pub(crate) fn too_long() -> Error {
+        Error { kind: Kind::TooLong }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.kind {
+            Kind::InvalidName => {
+                f.write_str("cookie name contains invalid character")
+            },
+            Kind::InvalidValue => {
+                f.write_str("cookie value contains invalid character")
+            },
+            Kind::InvalidPath => {
+                f.write_str("cookie path is invalid")
+            },
+            Kind::InvalidDomain => {
+                f.write_str("cookie domain is invalid")
+            },
+            Kind::TooLong => {
+                f.write_str("cookie string is too long")
+            },
+        }
+    }
+}
+
+impl std::error::Error for Error {}
--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,13 @@ use std::fmt;
 use std::time::Duration;
 
 mod build;
+mod error;
 mod parse;
 mod util;
 
 use self::sealed::Sealed;
 pub use self::build::Builder;
+pub use self::error::Error;
 pub use self::parse::parse;
 
 /// Cookies in this crate implement this trait.
@@ -44,69 +46,6 @@ pub trait Cookie: fmt::Debug + fmt::Display + Sealed {
     /// Get if the `SameSite=Lax` attribute was on this cookie.
     fn same_site_lax(&self) -> bool;
 }
-
-/// Error type when failing to parse or build a `Cookie`.
-#[derive(Debug)]
-pub struct Error {
-    kind: Kind,
-}
-
-#[derive(Debug)]
-enum Kind {
-    InvalidName,
-    InvalidValue,
-    InvalidPath,
-    InvalidDomain,
-    TooLong,
-}
-
-// ===== impl Error =====
-
-impl Error {
-    fn invalid_name() -> Error {
-        Error { kind: Kind::InvalidName }
-    }
-
-    fn invalid_value() -> Error {
-        Error { kind: Kind::InvalidValue }
-    }
-
-    fn invalid_path() -> Error {
-        Error { kind: Kind::InvalidPath }
-    }
-
-    fn invalid_domain() -> Error {
-        Error { kind: Kind::InvalidDomain }
-    }
-
-    fn too_long() -> Error {
-        Error { kind: Kind::TooLong }
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.kind {
-            Kind::InvalidName => {
-                f.write_str("cookie name contains invalid character")
-            },
-            Kind::InvalidValue => {
-                f.write_str("cookie value contains invalid character")
-            },
-            Kind::InvalidPath => {
-                f.write_str("cookie path is invalid")
-            },
-            Kind::InvalidDomain => {
-                f.write_str("cookie domain is invalid")
-            },
-            Kind::TooLong => {
-                f.write_str("cookie string is too long")
-            },
-        }
-    }
-}
-
-impl std::error::Error for Error {}
 
 mod sealed {
     pub trait Sealed {}
